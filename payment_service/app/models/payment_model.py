65d21b8a98cb482749b6d3b7f8dd37f87c619214
Muhammad Haris <mharis01470017@gmail.com>
2024-07-06 15:56:05 +0500

    update

---
+++ payment_service/app/models/payment_model.py
@@ -0,0 +1,36 @@
+from typing import Optional
+from sqlmodel import Relationship, SQLModel, Field
+
+class RemainingPayment(SQLModel):
+    order_id: int = Field(foreign_key = "order.order_id")
+    total_price: int
+    payment_method: Optional[str]
+    payment_method_id: str
+    remaining_balance: float
+
+class PaymentForm(SQLModel):
+    order_id: int = Field(foreign_key = "order.order_id")
+    total_price: int
+    payment_method: Optional[str]
+    payment_method_id: Optional[str]
+    advance_payment: Optional["AdvancePayment"]
+
+class Payment(SQLModel, table = True):
+    order_id: int = Field(foreign_key = "order.order_id")
+    total_price: int
+    payment_method: Optional[str]
+    payment_id: int = Field(primary_key = True)
+    payment_intent_id: Optional[str]
+    payment_status: str
+    remaining_balance: float = Field(default = 0)
+    is_completed: bool = Field(default = False)
+    advance_payment: Optional["AdvancePayment"] = Relationship(back_populates = "payment")
+
+class AdvancePayment(SQLModel, table=True):
+    advance_price: int
+    advance_payment_method: str
+    advance_payment_id: Optional[int] = Field(primary_key = True)
+    payment_id: int = Field(foreign_key = "payment.payment_id")
+    advance_payment_intent_id: str
+    advance_payment_status: str
+    payment: Optional[Payment] = Relationship(back_populates = "advance_payment")
